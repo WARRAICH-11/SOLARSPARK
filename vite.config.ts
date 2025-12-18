9ce15e891ae7a2d5cbc5b9b443b8553670304195
HASSAN WARRAICH <127755243+WARRAICH-11@users.noreply.github.com>
2025-12-18 06:15:35 +0000

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   // IMPORTANT: This matches your GitHub repository name
-  base: "/SOLARSPARK/", 
+  base: "/SOLARSPARK/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -16,7 +16,7 @@ export default defineConfig({
   root: path.resolve(__dirname, "client"),
   build: {
     // Tells Vite to put the final website in a folder called 'dist' at the root
-    outDir: path.resolve(__dirname, "dist"),
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
 });
